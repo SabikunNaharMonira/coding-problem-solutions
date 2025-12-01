8d21ace5fee2a8cdec943d5d7fc7574d6ccc4b64
Sabikun Nahar Monira <sabikunnaharmoniraofficial@gmail.com>
2025-12-01 09:35:15 +0600

    Create 50A domino piling.cpp

---
+++ Codeforce/50A domino piling.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    int count;
+    int n =0, m =0;
+    cin >> n >> m ;
+    int area = n*m;
+    cout << (area/2) << endl;
+    return 0;
+}
